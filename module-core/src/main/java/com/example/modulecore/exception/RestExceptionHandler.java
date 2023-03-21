d4ab5d520ed8c07120012aef9946bf58c12b12ce
junho <wnsgh1357@gmail.com>
2023-03-22 01:32:01 +0900

    Annotation 수정

--- module-core/src/main/java/com/example/modulecore/exception/RestExceptionHandler.java
+++ module-core/src/main/java/com/example/modulecore/exception/RestExceptionHandler.java
@@ -9,10 +9,10 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(AllSearchProvidersFailedException.class)
