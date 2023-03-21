49ca52b2efcc3cb30f00932080952f76a1c4b616
junho <wnsgh1357@gmail.com>
2023-03-21 16:15:01 +0900

    gradle 설정 최소화

--- module-client/src/main/java/com/example/moduleclient/ModuleClientApplication.java
+++ module-client/src/main/java/com/example/moduleclient/ModuleClientApplication.java
@@ -2,12 +2,7 @@ package com.example.moduleclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-
 
 @SpringBootApplication(
         scanBasePackages = { "com.example.moduleclient", "com.example.modulecore", "com.example.moduleexternalapi" }
